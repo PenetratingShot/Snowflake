a6c15cf3d4a681d32c1ac23e8c327f03e0660f48
Shreyas Lad <shadowtemplates@gmail.com>
2017-02-06 21:59:27 -0800

    FadeIn, FadeOut

--- dist/snowflake.js
+++ dist/snowflake.js
@@ -1,1 +1,20 @@
+//FadeIn .fadeIn
+$(document).ready(function(){
+  $('body').load(function(){
+    $('.fadeIn').fadeIn('3000');
+  });
+});
 
+//FadeIn .fadeIn .btn1 (button)
+$(document).ready(function(){
+  $('.btn').click(function(){
+    $('.fadeIn').fadeIn('3000');
+  });
+});
+
+//FadeOut .fadeOut .btn2 (button)
+$(document).ready(function(){
+  $('.btn2').click(function(){
+    $('fadeOut').fadeOut('3000');
+  });
+});
